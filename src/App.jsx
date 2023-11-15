0afcdccc30cecfdec9b45dc6c0490f1e8e91f14b
Julian Ng <julesnuggy@gmail.com>
2023-11-15 12:13:33 +0000

    Nikolaos Litsas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
+import { createContext } from 'react'
+import CreateTweet from './components/CreateTweet.jsx'
+import Tweet from './components/Tweet.jsx'
 
+const ThemeContext = createContext()
+const DataContext = createContext()
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
     const [theme, setTheme] = useState('light');
@@ -16,14 +21,18 @@ function App() {
     }, [theme])
 
     return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
+        <ThemeContext.Provider value = {theme}>
+            <DataContext.Provider value = {{ user: user, tweets: tweets, setTweets: setTweets }}>
+                <CreateTweet/>
+                <Header/>
+                <RightSide/>
+                <Tweet/>
+                <Tweets/>
+            </DataContext.Provider>
+        </ThemeContext.Provider>
     )
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { App, ThemeContext, DataContext };
--- src/components/CreateTweet.jsx
+++ src/components/CreateTweet.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import imgDoge from '../assets/images/doge.jpg'
+import { useContext } from 'react'
+import { DataContext, ThemeContext } from '../App'
 
-export default function CreateTweet({ tweets, setTweets, user, theme }) {
+export default function CreateTweet() {
     const [content, setContent] = useState('')
+    const dataContext = useContext(DataContext)
+    const themeContext = useContext(ThemeContext)
+
 
     const addTweet = (e) => {
         e.preventDefault()
@@ -16,12 +21,12 @@ export default function CreateTweet({ tweets, setTweets, user, theme }) {
                 heartCount: 0,
                 analyticsCount: 0
             },
-            ...tweets
+            ...dataContext.tweets
         ])
     }
 
     return (
-        <div className={theme === 'dark' ? 'create-tweet dark' : 'create-tweet'}>
+        <div className={themeContext === 'dark' ? 'create-tweet dark' : 'create-tweet'}>
             <form onSubmit={addTweet}>
                 <div className="avatar-section">
                     <div className="profile-icon"><img src={imgDoge}/></div>
