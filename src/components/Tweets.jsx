95cfd2d495bd60ca103d11e55cfa2e087dbe7f7b
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-11-17 00:14:03 +0100

    Part one done

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,20 +1,30 @@
-import CreateTweet from './CreateTweet'
-import Tweet from './Tweet'
+import CreateTweet from "./CreateTweet";
+import Tweet from "./Tweet";
+import { MyContext } from "../App";
+import { useContext } from "react";
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
+export default function Tweets({ theme }) {
+  const { tweets, setTweets, user } = useContext(MyContext);
   return (
-        <main>
-            <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
-                <h2 className="title">Home</h2>
-            </div>
+    <main>
+      <div className={theme === "dark" ? "top-bar dark" : "top-bar"}>
+        <h2 className="title">Home</h2>
+      </div>
 
-            <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+      <CreateTweet
+        tweets={tweets}
+        setTweets={setTweets}
+        user={user}
+        theme={theme}
+      />
 
-            <div className="show-more-tweets">
-                <p>Show 35 Tweets</p>
-            </div>
+      <div className="show-more-tweets">
+        <p>Show 35 Tweets</p>
+      </div>
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
-        </main>
-    )
+      {tweets.map((tweet, index) => (
+        <Tweet tweet={tweet} theme={theme} key={index} />
+      ))}
+    </main>
+  );
 }
