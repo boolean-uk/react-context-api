2c065dec62087bd61a37c948bfddb755a0782e8e
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-16 23:42:35 +0100

    I have one problem

--- src/components/CreateTweet.jsx
+++ src/components/CreateTweet.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import imgDoge from '../assets/images/doge.jpg'
+import { UserContext } from '../App'
 
-export default function CreateTweet({ tweets, setTweets, user, theme }) {
+export default function CreateTweet() {
+
+    const {tweets, setTweets, user, theme} = useContext(UserContext)
     const [content, setContent] = useState('')
 
     const addTweet = (e) => {
