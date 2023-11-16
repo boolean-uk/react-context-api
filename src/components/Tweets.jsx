c9173b9e396aacaa43ada21b2ce41e75a5e1b6f0
Lukas Dembicki <lad_777@hotmail.com>
2023-11-16 11:51:31 +0000

    removed props: user, tweets, setTweets and replaced with UserContext. Made correct changed to components.

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,8 +1,13 @@
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { UserContext } from '../App'
+import { useContext } from 'react'
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
-  return (
+export default function Tweets({ theme }) {
+
+    const { tweets, user, setTweets } = useContext(UserContext)
+
+    return (
         <main>
             <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
                 <h2 className="title">Home</h2>
