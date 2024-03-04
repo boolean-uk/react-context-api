f0c3c2ab49d2ee160e85d0a96f326a26909682b2
llllllll-l <vincent.werelius@gmail.com>
2024-03-04 10:19:58 +0100

    node modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Tweets from "./components/Tweets";
+import RightSide from "./components/RightSide";
+import defaultTweets from "./assets/data/tweets.js";
+import user from "./assets/data/user.js";
 
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const [theme, setTheme] = useState("light");
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === "light"
+      ? (document.body.style.backgroundColor = "white")
+      : (document.body.style.backgroundColor = "black");
+  }, [theme]);
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <div className="container">
+      <Header user={user} theme={theme} setTheme={setTheme} />
+      <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+      <RightSide theme={theme} />
+    </div>
+  );
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
-// more than one thing to export from this file.
+// more than one thing to export from this file. wow
 export { App };
