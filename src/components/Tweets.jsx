eec81c0d13aa4c6dc2924f96a0e068a7b3834342
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-03-05 10:02:52 +0100

    done with the first part

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { TwitterContext } from '../App'
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
+export default function Tweets() {
+  const { tweets, setTweets, user, theme }  = useContext(TwitterContext)
+  
   return (
         <main>
             <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
@@ -14,7 +18,9 @@ export default function Tweets({ tweets, setTweets, user, theme }) {
                 <p>Show 35 Tweets</p>
             </div>
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
+            {tweets.map((tweet, index) => 
+            <Tweet tweet={tweet} key={index} />
+            )}
         </main>
     )
 }
