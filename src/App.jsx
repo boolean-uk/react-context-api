0d81a1df1f330e19cd6cac852638ab51334a99e5
Caroline Berg <caroline.berg@no.experis.com>
2024-03-06 10:06:45 +0100

    context and local storage exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, createContext } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
 
+const AppContext = createContext()
+const ThemeContext = createContext()
+
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme')?localStorage.getItem('theme'):'light');
 
     useEffect(() => {
         theme === 'light'
@@ -17,13 +20,16 @@ function App() {
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+            <AppContext.Provider value={{ appName: 'Twitter', tweets: tweets, setTweets: setTweets, user:user}}>
+            <ThemeContext.Provider value={{theme:theme, setTheme:setTheme}}>
+            <Header />
+            <Tweets  />
+            <RightSide theme={theme} /></ThemeContext.Provider>
+            </AppContext.Provider>
         </div>
     )
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { App, AppContext ,ThemeContext};
