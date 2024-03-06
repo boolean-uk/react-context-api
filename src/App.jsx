0b9269a32e1bf98d112d24399c7871da828fad02
stp <steventphung6@gmail.com>
2024-03-06 13:04:04 +0100

    Steven Tong Phung

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, createContext } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
 
+const MyContext = createContext();
+const MyTheme = createContext();
+
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
     const [theme, setTheme] = useState('light');
 
     useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+        if (localStorage.getItem("theme")) {
+            setTheme(localStorage.getItem("theme"));
+          }
+          if (theme === "light") {
+            (document.body.style.backgroundColor = "white")
+          } else {
+            (document.body.style.backgroundColor = "black")
+          }
+        }, [theme]);
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+            <MyTheme.Provider value={{theme, setTheme}}>
+                <MyContext.Provider value={{user, tweets, setTweets}}>
+                    <Header/>
+                    <Tweets />
+                    <RightSide /> 
+                </MyContext.Provider>
+            </MyTheme.Provider>
         </div>
     )
 }
+//<Header user={user} theme={theme} setTheme={setTheme} />
+// <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
+// <RightSide theme={theme} /> 
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { MyTheme, MyContext ,App };
