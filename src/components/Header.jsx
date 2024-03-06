ccb780ecdcd5e29f73ba7c6937d07d30d8bfc32d
Maha Riazi <maha.riazi@hotmail.no>
2024-03-06 11:36:53 +0100

    complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,114 +1,134 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Context, ThemeContext } from "../App";
 
 export default function Header() {
-    const { user } = useContext(Context)
-    const { theme, setTheme } = useContext(ThemeContext)
-    
-    const handleCheckChange = () => {
-      if(theme === 'dark') {
-        setTheme('light');
-      } else {
-        setTheme('dark');
-      }
+  const { user } = useContext(Context);
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    const storageTheme = localStorage.getItem("theme")
+    storageTheme ? setTheme(storageTheme) : setTheme("light")
+  }, [setTheme])
+
+  const handleCheckChange = () => {
+    let newTheme;
+
+    if (theme === "dark") {
+      newTheme = "light";
+    } else {
+      newTheme = "dark";
     }
 
-    const handleButtonClick = () => {
-      console.log("CLICK!");
-    }
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
-    return (
-        <header className={theme}>
-            <div>
-                <div className="dark-mode-container">
-                    <input id="darkMode" type="checkbox" checked={theme === 'dark'} onChange={handleCheckChange}></input>
-                    <label htmlFor="darkMode">Enable Dark Mode</label>
-                </div>
-                <div>
-                    <button className="clear-settings-btn" onClick={handleButtonClick}>Clear Locally Saved Settings</button>
-                </div>
-            </div>
-            <div className="logo">
-                <i className="fa-brands fa-twitter"></i>
-            </div>
+  const handleButtonClick = () => {
+    localStorage.removeItem("theme")
+    setTheme("light")
+  };
 
-            <div className="menu-item active">
-                <a href="#">
-                    <i className="fa-solid fa-house"></i>
-                    Home
-                </a>
-            </div>
+  return (
+    <header className={theme}>
+      <div>
+        <div className="dark-mode-container">
+          <input
+            id="darkMode"
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={handleCheckChange}
+          ></input>
+          <label htmlFor="darkMode">Enable Dark Mode</label>
+        </div>
+        <div>
+          <button className="clear-settings-btn" onClick={handleButtonClick}>
+            Clear Locally Saved Settings
+          </button>
+        </div>
+      </div>
+      <div className="logo">
+        <i className="fa-brands fa-twitter"></i>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                    Explore
-                </a>
-            </div>
+      <div className="menu-item active">
+        <a href="#">
+          <i className="fa-solid fa-house"></i>
+          Home
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-regular fa-bell"></i>
-                    Notifications
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          Explore
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-regular fa-envelope"></i>
-                    Messages
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-bell"></i>
+          Notifications
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-solid fa-bars"></i>
-                    Lists
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-envelope"></i>
+          Messages
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-regular fa-bookmark"></i>
-                    Bookmarks
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-bars"></i>
+          Lists
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-regular fa-circle-check"></i>
-                    Verified
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-bookmark"></i>
+          Bookmarks
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-regular fa-user"></i>
-                    Profile
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-circle-check"></i>
+          Verified
+        </a>
+      </div>
 
-            <div className="menu-item">
-                <a href="#">
-                    <i className="fa-solid fa-ellipsis"></i>
-                    More
-                </a>
-            </div>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-user"></i>
+          Profile
+        </a>
+      </div>
 
-            <button className="tweet-btn">Tweet</button>
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-ellipsis"></i>
+          More
+        </a>
+      </div>
 
-            <div className={theme === 'dark' ? 'profile-card dark' : 'profile-card'}>
-                <div className="profile-icon"><img src={user.profileImage}/></div>
+      <button className="tweet-btn">Tweet</button>
 
-                <div className="profile-details">
-                    <h4>{user.name}</h4>
-                    <small>{user.handle}</small>
-                </div>
+      <div className={theme === "dark" ? "profile-card dark" : "profile-card"}>
+        <div className="profile-icon">
+          <img src={user.profileImage} />
+        </div>
 
-                <div className="action">
-                    <i className="fa-solid fa-ellipsis"></i>
-                </div>
-            </div>
-        </header>
-    )
+        <div className="profile-details">
+          <h4>{user.name}</h4>
+          <small>{user.handle}</small>
+        </div>
+
+        <div className="action">
+          <i className="fa-solid fa-ellipsis"></i>
+        </div>
+      </div>
+    </header>
+  );
 }
