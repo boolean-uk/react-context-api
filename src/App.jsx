cfc22233d5b0f2cb91fbc6af48b588029a9bed0b
Petros Mylonas <petrosmylonascs@gmail.com>
2024-03-06 20:53:34 +0200

    Petros Mylonas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
+import React, { useEffect, useState, createContext } from 'react';
+import Header from './components/Header';
+import Tweets from './components/Tweets';
+import RightSide from './components/RightSide';
+import defaultTweets from './assets/data/tweets.js';
+import user from './assets/data/user.js';
+
+const UserContext = createContext();
+const TweetsContext = createContext();
+const ThemeContext = createContext();
 
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const initTheme = localStorage.getItem('theme') || 'light';
+  const [theme, setTheme] = useState(initTheme);
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === 'light'
+      ? document.body.classList.remove('dark-theme')
+      : document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <div className="container">
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <UserContext.Provider value={user}>
+          <TweetsContext.Provider value={{ tweets, setTweets }}>
+            <Header />
+            <Tweets />
+            <RightSide />
+          </TweetsContext.Provider>
+        </UserContext.Provider>
+      </ThemeContext.Provider>
+    </div>
+  );
 }
 
-// NOTE! Instead of `export default App` we use `export { App }` here because we have
-// more than one thing to export from this file.
-export { App };
+export { App, UserContext, TweetsContext, ThemeContext };
