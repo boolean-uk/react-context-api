60520cadf4ed735a9566fbb450fa10a8050960c5
Sebastian <sebastian.thunberg@hotmail.se>
2024-03-06 13:19:47 +0100

    Sebastian Thunberg

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
-
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Tweets from "./components/Tweets";
+import RightSide from "./components/RightSide";
+import defaultTweets from "./assets/data/tweets.js";
+import user from "./assets/data/user.js";
+import { createContext } from "react";
+const Context = createContext();
+const ThemeContext = createContext();
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === "light"
+      ? (document.body.style.backgroundColor = "white")
+      : (document.body.style.backgroundColor = "black");
+  }, [theme]);
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className="container">
+        <Context.Provider value={{ tweets, setTweets, user }}>
+          <Header />
+          <Tweets />
+        </Context.Provider>
+        <RightSide />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { App, Context, ThemeContext };
