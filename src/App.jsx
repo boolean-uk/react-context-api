e87ec98242b318c49df7b57b5df84cf11f3a11ea
Axel Hansson <axel.co.hansson@gmail.com>
2024-03-06 13:34:12 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, createContext } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
 
+const PostContext = createContext()
+const ThemeContext = createContext()
+
+
+function getThemeState () {
+    const mode = localStorage.getItem("mode")
+    return (mode) ? mode : "light"
+}
+
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(getThemeState);
 
     useEffect(() => {
         theme === 'light'
@@ -17,13 +26,20 @@ function App() {
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
+            <ThemeContext.Provider value={{theme: theme, setTheme: setTheme}}>
+            <RightSide/>
+            <PostContext.Provider value={{user: user, tweets: tweets, setTweets: setTweets}}>
+                <Header/>
+                <Tweets/>
+            </PostContext.Provider>
+            </ThemeContext.Provider>
+            {/* <Header user={user} theme={theme} setTheme={setTheme} />
             <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+            <RightSide theme={theme} /> */}
         </div>
     )
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { App, PostContext, ThemeContext };
--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,20 +1,25 @@
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { useContext } from 'react'
+import { PostContext, ThemeContext } from '../App'
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
+export default function Tweets() {
+    const postContext = useContext(PostContext)
+    const themeContext = useContext(ThemeContext)
+
   return (
         <main>
-            <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
+            <div className={themeContext.theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
                 <h2 className="title">Home</h2>
             </div>
 
-            <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+            <CreateTweet tweets={postContext.tweets} setTweets={postContext.setTweets} user={postContext.user} theme={themeContext.theme} />
 
             <div className="show-more-tweets">
                 <p>Show 35 Tweets</p>
             </div>
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
+            {postContext.tweets.map((tweet, index) => <Tweet tweet={tweet} theme={themeContext.theme} key={index} />)}
         </main>
     )
 }
