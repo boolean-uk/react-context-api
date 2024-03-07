2317a3b21abea9781e9bbd1e5ae232ef31389a0f
Mateusz Pietrzak <matti3301@gmail.com>
2024-03-07 09:59:38 +0100

    Completed all tasks

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,20 +1,24 @@
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { DataContext, ThemeContext } from "../App"
+import { useContext } from "react"
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
+export default function Tweets() {
+    const dataContext = useContext(DataContext)
+    const themeContext = useContext(ThemeContext)
   return (
         <main>
-            <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
+            <div className={themeContext.theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
                 <h2 className="title">Home</h2>
             </div>
 
-            <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+            <CreateTweet tweets={dataContext.tweets} setTweets={dataContext.setTweets} user={dataContext.user} theme={themeContext.theme} />
 
             <div className="show-more-tweets">
                 <p>Show 35 Tweets</p>
             </div>
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
+            {dataContext.tweets.map((tweet, index) => <Tweet tweet={tweet} theme={themeContext.theme} key={index} />)}
         </main>
     )
 }
