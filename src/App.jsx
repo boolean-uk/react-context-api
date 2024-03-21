f4ab8b4226d2bfa398eb375f2db215439ce291bd
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-03-21 21:01:04 +0100

    Contexts: Added userContext and tweetsContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
+import { createContext, useEffect, useState } from "react";
+import Header from "./components/Header";
+import Tweets from "./components/Tweets";
+import RightSide from "./components/RightSide";
+import defaultTweets from "./assets/data/tweets.js";
+import user from "./assets/data/user.js";
+
+const userContext = createContext();
+const tweetsContext = createContext();
 
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const [theme, setTheme] = useState("light");
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === "light" ? (document.body.style.backgroundColor = "white") : (document.body.style.backgroundColor = "black");
+  }, [theme]);
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <userContext.Provider value={{ user }}>
+      <div className="container">
+        <Header theme={theme} setTheme={setTheme} />
+        <tweetsContext.Provider value={{ tweets, setTweets }}>
+          <Tweets theme={theme} />
+        </tweetsContext.Provider>
+        <RightSide theme={theme} />
+      </div>
+    </userContext.Provider>
+  );
 }
 
 // NOTE! Instead of `export default App` we use `export { App }` here because we have
 // more than one thing to export from this file.
-export { App };
+export { App, userContext, tweetsContext };
