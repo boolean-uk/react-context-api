79226dddca78d387c52ffb3957dced25098129e2
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-05-27 06:03:46 +0100

    Context

--- src/components/CreateTweet.jsx
+++ src/components/CreateTweet.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import imgDoge from '../assets/images/doge.jpg'
+import { AppContext, ThemeContext } from '../App'
 
-export default function CreateTweet({ tweets, setTweets, user, theme }) {
+export default function CreateTweet() {
     const [content, setContent] = useState('')
+    const {theme} = useContext(ThemeContext)
+    const {tweets, setTweets, user} = useContext(AppContext)
 
     const addTweet = (e) => {
         e.preventDefault()
